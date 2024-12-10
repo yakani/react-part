89940d376773d1a81e5f1f19d021be6846156495
yakani <yakamdimitri@gmail.com>
2024-12-10 01:56:07 +0100

    feature

--- src/component/Feature.jsx
+++ src/component/Feature.jsx
@@ -8,7 +8,9 @@ const Feature = ({ishome=false ,product}) => {
   const [goals,setgoals] = useState([]);
 useEffect(()=>{
 try {
-  const data = ()=>product();
+
+  const data = product;
+  
   setgoals(data);
 } catch (error) {
   console.log(error)
